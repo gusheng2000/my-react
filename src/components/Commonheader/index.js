f3ee7e41abc4ce3cbcffaa7be3cbcf5a8a734e2c
shikang <skr-1008@outlook.com>
2024-12-03 21:18:01 +0800

    简单布局header

--- src/components/Commonheader/index.js
+++ src/components/Commonheader/index.js
@@ -1,13 +1,37 @@
 import React from "react";
-import { Button} from "antd";
+import { Button,Avatar,Dropdown} from "antd";
 import { Header } from "antd/es/layout/layout";
-import {LeftCircleTwoTone} from '@ant-design/icons'
+import {MenuFoldOutlined} from '@ant-design/icons'
+import './index.css'
 const Commonheader=()=>{
+
+    const items = [
+        {
+          key: '1',
+          label: (
+            <a target="_blank" rel="noopener noreferrer">
+             个人中心
+            </a>
+          ),
+        },
+        {
+          key: '2',
+          label: (
+            <a target="_blank" rel="noopener noreferrer" >
+             退出
+            </a>
+          ),
+          disabled: false,
+        },
+      ];
     return (
-        <Header>
-            <Button>
-                <LeftCircleTwoTone />
+        <Header className="header-container">
+            <Button icon={<MenuFoldOutlined/>}>
             </Button>
+
+            <Dropdown  menu={{items}}>
+            <Avatar src={<img alt='头像' size={64}  src={require("../../assets/avatar.jpg")}></img>}></Avatar>
+            </Dropdown>
         </Header>
     )
 }
